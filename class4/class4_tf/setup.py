bd6634fc295558686ea2d989e726e236db4f2a60
cstick-ano <cstick987@gmail.com>
2023-07-24 01:45:24 +0700

    broadcaster

--- class4/class4_tf/setup.py
+++ class4/class4_tf/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'class4_tf'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -21,7 +24,13 @@ setup(
     entry_points={
         'console_scripts': [
             'assignment1 = class4_tf.assignment1:main',
-            ''
+            'broadcaster = class4_tf.broadcaster:main'
         ],
     },
 )
+
+data_files=[
+
+    (os.path.join('share', package_name, 'launch'), 
+     glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+],
